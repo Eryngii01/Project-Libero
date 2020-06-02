cbc993523bd44c55bebff6676e3e97dd1a5050ac
Shaelynn <80Currans@gmail.com>
2020-06-01 22:29:12 -0400

    Added unity game to the repository

---
+++ Not Your Father's Dungeon Crawler/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    // Start is called before the first frame update
+
+    public GameObject playerCamera;
+    public float speed;
+    private Vector3 cameraOffset = new Vector3(0, 0, -10);
+    Rigidbody2D rbody;
+    Animator anim;
+
+    void Start() {
+        rbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Make camera follow player
+        playerCamera.transform.position = transform.position + cameraOffset;
+
+        Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (movement_vector != Vector2.zero) {
+            anim.SetBool("is_walking", true);
+            anim.SetFloat("input_x", movement_vector.x);
+            anim.SetFloat("input_y", movement_vector.y);
+        } else {
+            anim.SetBool("is_walking", false);
+        }
+
+        rbody.MovePosition(rbody.position + movement_vector * speed);
+    }
+}
